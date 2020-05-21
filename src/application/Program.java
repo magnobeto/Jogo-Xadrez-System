29e80e78c124dc1a775fa533cf21e92abef1f118
Roberto Magno <magnobeto.rm@gmail.com>
2020-05-20 21:31:56 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+
+package application;
+/**
+ *
+ * @author Roberto
+ */
+public class Program {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
